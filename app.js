9d5578446f675eed862af377ecda6946d6a7389f
afsal <madathimgalafsal@gmail.com>
2024-03-29 18:48:18 +0530

    ip change

--- app.js
+++ app.js
@@ -28,12 +28,6 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use((req, res, next) => {
-  const clientIP = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  console.log(`Client IP: ${clientIP}`);
-  next();
-});
-
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'hbs')
